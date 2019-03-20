35ad8d5868b270cfe3cb1041909d4c824e0d7153
Ramdoni <doni.enginer@gmail.com>
2019-03-20 18:49:27 +0700

    Update payroll

--- public/admin-css/js/jquery.orgchart/jquery.orgchart.js
+++ public/admin-css/js/jquery.orgchart/jquery.orgchart.js
@@ -222,7 +222,8 @@
                 descString = '<p>'+self.data.description+'</p>';
             }
             if(opts.showControls){
-                var buttonsHtml = "<div class='org-add-button'><i class=\"fa fa-plus\"></i> "+opts.newNodeText+"</div><div class='org-del-button'><i class=\"fa fa-trash\"></i> </div>";
+                var buttonsHtml = "<div class='org-add-button'><i class=\"fa fa-plus\"></i> "+opts.newNodeText+"</div>";
+                    buttonsHtml += "<div class='org-del-button'><i class=\"fa fa-trash\"></i> </div>";
             }
             else{
                 buttonsHtml = '';
--- resources/views/administrator/request-pay-slip/print-pay-slip.blade.php
+++ resources/views/administrator/request-pay-slip/print-pay-slip.blade.php
@@ -80,13 +80,13 @@
 						<td>BPJS Jaminan Hari Tua (JHT) {{ get_setting('bpjs_jaminan_jht_employee') }}% (Employee) </td>
 						<td style="text-align: right;">{{ number_format($item->salary * get_setting('bpjs_jaminan_jht_employee') / 100) }}</td>
 					</tr>
-					<tr>
+					<tr> 
 						<td>BPJS Kesehatan ({{ get_setting('bpjs_kesehatan_employee') }}%) (employee)</td>
-						<td style="text-align: right;">{{ number_format($item->salary * get_setting('bpjs_kesehatan_employee') / 100) }}</td>
+						<td style="text-align: right;">{{ number_format($item->bpjs_kesehatan_employee) }}</td>
 					</tr>
 					<tr>
 						<td>BPJS Jaminan Pensiun (JP) {{ get_setting('bpjs_jaminan_jp_employee') }}% (Employee)</td>
-						<td style="text-align: right;"> {{ number_format($item->salary * get_setting('bpjs_jaminan_jp_employee') / 100) }} </td>
+						<td style="text-align: right;"> {{ number_format($item->bpjs_pensiun_employee) }} </td>
 					</tr>
 					<tr>
 						<td>PPH21</td>
