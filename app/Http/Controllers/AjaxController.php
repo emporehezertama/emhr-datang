ba5e9a4e0306d719eb8281d4a759fc99f2335400
damerias <dameriasimatupang@gmail.com>
2019-07-15 17:46:00 +0700

    update ajax getAdministrator

--- app/Http/Controllers/AjaxController.php
+++ app/Http/Controllers/AjaxController.php
@@ -1140,12 +1140,13 @@ class AjaxController extends Controller
             if($user->project_id != NULL)
             {
                  $data =  User::where('access_id', 2)->where('project_id', $user->project_id)->where(function($query){
-                    $query->where('name', 'LIKE', "%". $request->name . "%")->orWhere('nik', 'LIKE', '%'. $request->name .'%')
+                    $query->where('name', 'LIKE', "%". $request->name . "%")->orWhere('nik', 'LIKE', '%'. $request->name .'%');
                  })->get();
             } else{
                  $data =  User::where('access_id', 2)->where(function($query){
-                     $query->where('name', 'LIKE', "%". $request->name . "%")->orWhere('nik', 'LIKE', '%'. $request->name .'%')
+                     $query->where('name', 'LIKE', "%". $request->name . "%")->orWhere('nik', 'LIKE', '%'. $request->name .'%');
                  })->get();
+                 
             }
             $params = [];
             foreach($data as $k => $item)
