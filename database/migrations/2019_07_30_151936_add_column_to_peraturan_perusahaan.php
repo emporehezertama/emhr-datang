3501a319d03f7484ea8788c047babf60f854ce39
Ramdoni <doni.enginer@gmail.com>
2019-07-31 04:36:02 +0000

    Update live server

---
+++ database/migrations/2019_07_30_151936_add_column_to_peraturan_perusahaan.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddColumnToPeraturanPerusahaan extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('peraturan_perusahaan', function (Blueprint $table) {
+            $table->integer('status')->nullable()->after('content');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('peraturan_perusahaan', function (Blueprint $table) {
+            //
+        });
+    }
+}
