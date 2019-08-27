a14aa7b23440e68ba82b3fd06176d07752455527
Farros Siregar <farros.jackson@gmail.com>
2019-08-27 16:32:26 +0700

    update payroll totab bpjs company2

--- app/Http/Controllers/Administrator/PayrollController.php
+++ app/Http/Controllers/Administrator/PayrollController.php
@@ -416,7 +416,7 @@ class PayrollController extends Controller
             $params[$k]['BPJS Jaminan Hari Tua (JHT) (Employee) '. get_setting('bpjs_jaminan_jht_employee').'%']= $item->bpjs_ketenagakerjaan_employee;
             $params[$k]['BPJS Kesehatan (Employee) '. get_setting('bpjs_kesehatan_employee').'%']               = $item->bpjs_kesehatan_employee; //$item->salary *  get_setting('bpjs_kesehatan_employee') / 100;
             $params[$k]['BPJS Jaminan Pensiun (JP) (Employee) '. get_setting('bpjs_jaminan_jp_employee').'%']   = $item->bpjs_pensiun_employee;
-            $params[$k]['Total BPJS (Company) ']   = Payroll::where('payroll_id', $item->id)->latest()->first()->bpjstotalearning;
+            $params[$k]['Total BPJS (Company) ']   = Payroll::where('id', $item->id)->latest()->first()->bpjstotalearning;
             
             $params[$k]['Total Deduction (Burden + BPJS)']      = $item->total_deduction;
             $params[$k]['Monthly Income Tax (Employee)']                    = $item->pph21;
