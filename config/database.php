f13fce3e7084ca2ecf5380152a55663c6baf9181
Empore <doni.enginer@gmail.com>
2019-09-09 16:30:26 +0700

    commit sas.datang.co.id

--- config/database.php
+++ config/database.php
@@ -44,9 +44,9 @@ return [
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
 
-            'database' => env('DB_DATABASE', 'empore'),
+            'database' => env('DB_DATABASE', 'sasemhr'),
             'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
+            'password' => env('DB_PASSWORD', 'Rangkaibumi10'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
